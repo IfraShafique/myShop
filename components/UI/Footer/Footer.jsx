da231ecad8ad0ae6310495542b3f4d1a9b32feb2
Ifra Shafique <86965873+IfraShafique@users.noreply.github.com>
2024-02-06 23:39:45 +0500

    Committed

--- components/UI/Footer/Footer.jsx
+++ components/UI/Footer/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faSquareFacebook} from "@fortawesome/free-solid-svg-icons";
+import { faFacebookF } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   return (
-    <div className='py-20 w-[90%] text-white px-10'>
-        <div className='flex'>
-            <div className='w-[70%]  sm:text-xl text-xs'>
+    <>
+    <div className=' pt-20 w-[90%] text-white px-10'>
+        <div className='flex max-sm:flex max-sm:flex-col'>
+            <div className='sm:w-[70%]   sm:text-xl text-xs'>
                 <ul className='flex'> 
                     <li className='mb-12 border-l-4 border-pink-800 px-2'>About Us</li>
                     <li className='mb-12 border-l-4 border-pink-800 px-2'>Contact Us</li>
@@ -17,15 +17,19 @@ export default function Footer() {
             <div className=' sm:text-xl text-xs font-bold '>
                 <p>Be With Us</p>
                 <div className='flex'>
-            <ul className='flex'>
-                <li>
-                <FontAwesomeIcon icon={faSquareFacebook}
-                className="text-white sm:w-10 sm:text-[36px] w-6  mt-1" />
-                </li>
-            </ul>
+                    <ul className='flex'>
+                        <li>
+                            <FontAwesomeIcon icon={faFacebookF} className="text-white sm:w-10 sm:text-[36px] w-6 mt-1" />
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
+
     </div>
+        <div className="mt-10 max-sm:text-xs text-center bg-pink-800 w-[100%] text-white py-1">
+            &copy; 2024. All Rights Reserved
+        </div>
+        </>
   )
 }
